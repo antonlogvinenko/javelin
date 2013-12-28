4b80b350d5a4f63cc31d7291cbd56334a22c91d6
Anton Logvinenko <logvinenko@farpost.com>
2013-12-28 23:07:17 +1100

    Plans

--- Main.hs
+++ Main.hs
@@ -1,9 +1,10 @@
 import Control.Monad.Error (ErrorT(..), runErrorT)
 import Control.Monad.State.Lazy (state, State, runState)
 import Data.ByteString (pack, ByteString)
+import Data.Word (Word32)
 
-type ParseFun = Integer -> ByteString -> ((Either String Integer), ByteString)
-type Parser = ErrorT String (State ByteString) Integer
+type ParseFun = ClassDef -> ByteString -> ((Either String ClassDef), ByteString)
+type Parser = ErrorT String (State ByteString) ClassDef
 
 (-->) :: Parser -> ParseFun -> Parser
 x --> f = x >>= ErrorT . state . f
@@ -13,14 +14,27 @@ countAll init = runState $ runErrorT $ classFileFormat where
                 classFileFormat = return init
                                   --> increment --> increment --> breaking --> decrement
 
+data ClassDef = EmptyClassDef |
+                ClassDef {minorVersion :: Word32,
+                          majorVersion :: Word32}
+
+
 -- the following are all ParseFun functions:
-increment count bytes = (Right $ count + 1, bytes)
-decrement count bytes = (Right $ count - 1, bytes)
+increment count bytes = (Right $ count, bytes)
+decrement count bytes = (Right $ count, bytes)
 breaking count bytes = (Left "cake!", bytes)
 
+
 main = do
-  let c = fst $ countAll 0 $ pack [1, 2]
+  let c = fst $ countAll EmptyClassDef $ pack [1, 2]
   case c of
     Right v -> putStrLn "cake"
     Left v -> putStrLn "Broken!"
   putStrLn "asd"
+
+
+-- ClassDefinition type
+-- parser functions
+-- read file
+-- test
+-- export what?
