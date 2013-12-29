cdc4b4040a4116ec2b27dd1d55f31f16a2c27459
Anton Logvinenko <logvinenko@farpost.com>
2013-12-29 11:04:14 +1100

    Writing parser

--- Main.hs
+++ Main.hs
@@ -1,24 +1,28 @@
 import Control.Monad.Error (ErrorT(..), runErrorT)
 import Control.Monad.State.Lazy (state, State, runState)
-import Data.ByteString (pack, ByteString)
-import Data.Word (Word32)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS (take, drop, pack, unpack)
+import Data.Word (Word32, Word8)
 
 parse :: ByteString -> Either String ClassDef
 parse = fst . (runState . runErrorT $ classFileFormat) where
         x --> f = x >>= ErrorT . state . f
         classFileFormat = return EmptyClassDef
-                          --> increment --> increment --> breaking --> decrement
+                          --> increment --> magicNumber
 
 data ClassDef = EmptyClassDef |
                 ClassDef {minorVersion :: Word32,
                           majorVersion :: Word32}
 
-increment count bytes = (Right $ count, bytes)
-decrement count bytes = (Right $ count, bytes)
-breaking count bytes = (Left "cake!", bytes)
+increment cd bs = (Right $ cd, bs)
+breaking cd bs = (Left "cake!", bs)
 
+magicNumber cd bs = if BS.take 4 bs == BS.pack [0xCA, 0xFE, 0xBA, 0xBE]
+                    then (Right cd, BS.drop 4 bs)
+                    else (Left "Not a Java class format", bs)
+                      
 main = do
-  let c = parse $ pack [1, 2]
+  let c = parse $ BS.pack [0xCA, 0xFE, 0xBA, 0xBE]
   case c of
     Right v -> putStrLn "cake"
     Left v -> putStrLn "Broken!"
@@ -29,3 +33,4 @@ main = do
 -- read file
 -- test
 -- export what?
+-- module name, system
