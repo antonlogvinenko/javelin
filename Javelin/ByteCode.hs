f7817238327bcc8b59a0b9f122463e358f1fe144
Anton Logvinenko <logvinenko@farpost.com>
2013-12-29 13:39:21 +1100

    Module system

---
+++ Javelin/ByteCode.hs
@@ -0,0 +1,52 @@
+module Javelin.ByteCode (parse) where
+  
+import Control.Monad.Error (ErrorT(..), runErrorT)
+import Control.Monad.State.Lazy (state, State, runState)
+import Data.ByteString (ByteString, unpack)
+import Data.Word (Word32, Word16, Word8)
+
+parse :: [Word8] -> Either String ClassDef
+parse = fst . (runState . runErrorT $ classFileFormat) where
+        x --> f = x >>= ErrorT . state . f
+        classFileFormat = return EmptyClassDef -->
+                          magicNumber --> minorVersion --> majorVersion -->
+                          constantPoolCount --> constantPool
+
+data ConstantPoolInfo = ConstantPoolInfo deriving (Show)
+
+data ClassDef = EmptyClassDef |
+                ClassDef {minVer :: Word16,
+                          majVer :: Word16,
+                          constPoolSize :: Word16,
+                          constPool :: [ConstantPoolInfo]
+                         } deriving (Show)
+
+stub cd bs = (Right cd, bs)
+require len bs f = if length bs < len
+                   then (Left "Unexpected EOF", bs)
+                   else f
+upd2bytes bs cdUpd = require 2 bs $
+                 let high = bs !! 1
+                     low = bs !! 2
+                     ver = fromIntegral $ high * 8 + low
+                 in (Right $ cdUpd ver, drop 2 bs)
+                    
+magicNumber cd bs = if take 4 bs == [0xCA, 0xFE, 0xBA, 0xBE]
+                    then (Right cd, drop 4 bs)
+                    else (Left "Not a Java class format", bs)
+minorVersion cd bs = upd2bytes bs $ \v -> cd {minVer = v}
+majorVersion cd bs = upd2bytes bs $ \v -> cd {majVer = v}
+constantPoolCount cd bs = upd2bytes bs $ \v -> cd {constPoolSize = v}
+constantPool = stub
+
+
+
+-- use lenses for upd2bytes
+-- infrastructure: inform where the error happened - EOF when parsing WHAT?
+-- test
+-- export what?
+-- module name, system
+
+-- parser functions
+-- read file
+
--- Main.hs
+++ Main.hs
@@ -1,48 +1,13 @@
-import Control.Monad.Error (ErrorT(..), runErrorT)
-import Control.Monad.State.Lazy (state, State, runState)
-import Data.ByteString (ByteString, unpack)
-import Data.Word (Word32, Word16, Word8)
+import Javelin.ByteCode (parse)
 
-parse :: [Word8] -> Either String ClassDef
-parse = fst . (runState . runErrorT $ classFileFormat) where
-        x --> f = x >>= ErrorT . state . f
-        classFileFormat = return EmptyClassDef -->
-                          magicNumber --> minorVersion --> majorVersion -->
-                          stub
-
-data ClassDef = EmptyClassDef |
-                ClassDef {minVer :: Word16,
-                          majVer :: Word16} deriving (Show)
-
-require len bs f = if length bs < len
-                   then (Left "Unexpected EOF", bs)
-                   else f
-
-magicNumber cd bs = if take 4 bs == [0xCA, 0xFE, 0xBA, 0xBE]
-                    then (Right cd, drop 4 bs)
-                    else (Left "Not a Java class format", bs)
-
-version bs cdUpd = require 2 bs $
-                 let high = bs !! 1
-                     low = bs !! 2
-                     ver = fromIntegral $ high * 8 + low
-                 in (Right $ cdUpd ver, bs)
-minorVersion cd bs = version bs $ \x -> cd {minVer = x}
-majorVersion cd bs = version bs $ \x -> cd {majVer = x}
-
-stub cd bs = (Right cd, bs)
+bytecode = [0xCA, 0xFE, 0xBA, 0xBE,
+            0x00, 0x00,
+            0x00
+           ]
 
 main = do
-  let c = parse [0xCA, 0xFE, 0xBA, 0xBE, 0x00, 0x00]
+  let c = parse bytecode
   case c of
     Right cd -> putStrLn $ "Parsed ok :)"
     Left msg -> putStrLn $ "Failed with error: " ++ msg
   putStrLn "Complete"
-
-
--- infrastructure: inform where the error happened - EOF when parsing WHAT?
--- parser functions
--- read file
--- test
--- export what?
--- module name, system
