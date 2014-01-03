4bb222fa032981ea4ee921fe3f8e05a937da56c3
Anton Logvinenko <logvinenko@farpost.com>
2014-01-03 14:45:47 +1100

    Wrote some tests

--- src/Javelin/ByteCode.hs
+++ src/Javelin/ByteCode.hs
@@ -1,18 +1,26 @@
-module Javelin.ByteCode (parse, ClassDef) where
+module Javelin.ByteCode (parse,
+                         require, upd2bytes, stub,
+                         magicNumber, minorVersion, majorVersion,
+                         constantPoolCount, constantPool,
+                         ClassDef(..))
+where
 
 import Control.Monad.Error (ErrorT(..), runErrorT)
 import Control.Monad.State.Lazy (state, State, runState)
 import Data.ByteString (ByteString, unpack)
 import Data.Word (Word32, Word16, Word8)
 
+type ParsedByteCode = Either String ClassDef
+    
 -- | Transforms Java class bytecode into either error message or a class definition.
-parse :: [Word8] -> Either String ClassDef
+parse :: [Word8] -> ParsedByteCode
 parse = fst . (runState . runErrorT $ classFileFormat) where
     x --> f = x >>= ErrorT . state . f
     classFileFormat = return EmptyClassDef -->
-                      magicNumber --> minorVersion --> majorVersion --> constantPoolCount --> constantPool
+                      magicNumber --> minorVersion --> majorVersion -->
+                      constantPoolCount --> constantPool
 
-data ConstantPoolInfo = ConstantPoolInfo deriving (Show)
+data ConstantPoolInfo = ConstantPoolInfo deriving (Show, Eq)
 
 -- | Represents Java class file structure
 data ClassDef = EmptyClassDef |
@@ -20,16 +28,18 @@ data ClassDef = EmptyClassDef |
                           majVer :: Word16,
                           constPoolSize :: Word16,
                           constPool :: [ConstantPoolInfo]
-                         } deriving (Show)
+                         } deriving (Show, Eq)
 
 stub cd bs = (Right cd, bs)
+
+require :: Int -> [Word8] -> (ParsedByteCode, [Word8]) -> (ParsedByteCode, [Word8])
 require len bs f = if length bs < len
                    then (Left "Unexpected EOF", bs)
                    else f
 upd2bytes bs cdUpd = require 2 bs $
-                     let high = bs !! 1
-                         low = bs !! 2
-                         ver = fromIntegral $ high * 8 + low
+                     let high = bs !! 0
+                         low = bs !! 1
+                         ver = fromIntegral $ high * 256 + low
                      in (Right $ cdUpd ver, drop 2 bs)
 
 magicNumber cd bs = if take 4 bs == [0xCA, 0xFE, 0xBA, 0xBE]
--- test/Javelin/ByteCode/Test.hs
+++ test/Javelin/ByteCode/Test.hs
@@ -2,8 +2,30 @@ module Javelin.ByteCode.Test (byteCodeTest)
 where
 import Test.Tasty (testGroup, TestTree)
 import Test.Tasty.HUnit
+import Javelin.ByteCode
+import Data.Word (Word16)
 
-byteCodeTest = testGroup "bla" [testCase "cake test" someTest]
+byteCodeTest = testGroup "ByteCode parser test" [byteCodeParserTest,
+                                                requireTest
+                                                 ]
 
-someTest :: Assertion
-someTest = "abc" @=? "abc"
+requireTest = let t = (Left "error", [0x00])
+              in testGroup "require" [
+                      testCase "required" $
+                               require 2 [0x00, 0x00] t @=? t,
+                      testCase "nope" $
+                               require 3 [0x00, 0x00] t @=? (Left "Unexpected EOF", [0x00, 0x00])
+               ]
+
+byteCodeParserTest = testGroup "ByteCode parser" [
+                      testCase "parse" $
+                                   (parse bytecode) @=? (Left "Unexpected EOF")
+                     ]
+
+
+
+bytecode = [0xCA, 0xFE, 0xBA, 0xBE,
+            0x00, 0x00,
+            0x00, 0x00
+           ]
+            
