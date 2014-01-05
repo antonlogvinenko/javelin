3da73969ddb6b0bc13c0d976d1a7773f757ee03c
Anton Logvinenko <logvinenko@farpost.com>
2014-01-05 13:50:56 +1100

    Constant Pool parser

--- src/Javelin/ByteCode.hs
+++ src/Javelin/ByteCode.hs
@@ -4,16 +4,22 @@ where
 import Data.ByteString (ByteString, unpack)
 import Data.Word (Word32, Word16, Word8)
 import Control.Monad
-                   
+import Data.Map.Lazy
+
+-- Modeling java class format
 data ByteCode = ByteCode {minVer :: Word16, majVer :: Word16, body :: ClassBody}
                 deriving (Show, Eq)
-data ClassBody = ClassBody {constPool :: [ConstantPool],
+data ClassBody = ClassBody {constPool :: [Constant],
                            flags :: Word16,
                            this :: Word16,
                            super :: Word16}
                  deriving (Show, Eq)
-data ConstantPool = ConstantPool
-                    deriving (Show, Eq)
+
+data Constant = Utf8 | Integer | Float | Long | Double | Class | String |
+                Fieldref | Methodref | InterfaceMethodref | NameAndType |
+                MethodHandle | MethodType | InvokeDynamic
+              deriving (Show, Eq)
+
 
 data Field = Field deriving (Show, Eq)
 
@@ -21,8 +27,8 @@ data Attribute = Attribute deriving (Show, Eq)
 
 data Method = Method deriving (Show, Eq)
 
-type Parser a = [Word8] -> Either String ([Word8], a)
 
+-- Basic utility functions
 require :: Int -> [Word8] ->  a -> Either String a
 require len bs value = if length bs < len
                        then Left "Unexpected EOF"
@@ -34,6 +40,10 @@ getBytes count bs = require 2 bs $
                          ver = (fromIntegral high) * 256 + fromIntegral low
                      in (drop 2 bs, ver)
 
+type Parser a = [Word8] -> Either String ([Word8], a)
+
+
+-- Class file header functions
 magicNumber :: Parser Int
 magicNumber bs = if take 4 bs == [0xCA, 0xFE, 0xBA, 0xBE]
                  then Right (drop 4 bs, 42)
@@ -42,9 +52,37 @@ magicNumber bs = if take 4 bs == [0xCA, 0xFE, 0xBA, 0xBE]
 version :: Parser Word16
 version = getBytes 2
 
-getConstantPool :: Word16 -> Parser [ConstantPool]
-getConstantPool len bytes = Right (bytes, [])
 
+-- Constant pool
+constantTypeParser :: Map Int (Parser Constant)
+constantTypeParser = fromList [(1, constParser), (3, constParser), (4, constParser),
+                               (5, constParser), (6, constParser), (7, constParser),
+                               (8, constParser), (9, constParser), (10, constParser),
+                               (11, constParser), (12, constParser), (15, constParser),
+                               (16, constParser), (18, constParser)]
+getConstantParser :: Int -> Parser Constant
+getConstantParser idx = findWithDefault failingConstParser idx constantTypeParser
+
+constParser :: Parser Constant
+constParser bytes = Right (bytes, Utf8)
+
+failingConstParser :: Parser Constant
+failingConstParser _ = Left "Undefined constant"
+
+getConstant bytes = do
+  (bytes1, tag) <- getBytes 1 bytes
+  getConstantParser tag bytes1
+
+getConstantPool :: Word16 -> Parser [Constant]
+getConstantPool len bytes = do
+  if len == 1
+    then Right (bytes, [])
+    else do
+    (bytes1, pool) <- getConstant bytes
+    (bytes2, pools) <- getConstantPool (len - 1) bytes1
+    return (bytes2, (pool:pools))
+
+-- Interfaces
 getInterfaces :: Word16 -> Parser [Word16]
 getInterfaces len bytes = Right (bytes, [])
 
@@ -65,7 +103,7 @@ getCountAndList f bytes = do
 classBody :: Parser ClassBody
 classBody bytes = do
   (bytes1, pool) <- getCountAndList getConstantPool bytes
-  (bytes2, flags) <- getBytes 2 bytes
+  (bytes2, flags) <- getBytes 2 bytes1
   (bytes3, this) <- getBytes 2 bytes2
   (bytes4, super) <- getBytes 2 bytes3
   (bytes5, interfaces) <- getCountAndList getInterfaces bytes4
@@ -74,10 +112,14 @@ classBody bytes = do
   (bytesLast, attributes) <- getCountAndList getAttributes bytes7
   return (bytesLast, ClassBody pool flags this super)
 
+
+-- Parser of all things alive
 parse :: [Word8] -> Either String ByteCode
 parse bytes = do
   (bytes0, _) <- magicNumber bytes
   (bytes1, minor) <- version bytes0
   (bytes2, major) <- version bytes1
   (bytes3, body) <- classBody bytes2
-  if length bytes3 == 0 then Left "Bytes left" else return $ ByteCode minor major body
+  if length bytes3 == 0
+    then Left "Bytes left"
+    else return $ ByteCode minor major body
