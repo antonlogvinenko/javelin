3073512c8cae196aa5cb3e2d5a7328271b4cf3cc
Anton Logvinenko <logvinenko@farpost.com>
2014-01-05 15:54:32 +1100

    Moar parsers

--- src/Javelin/ByteCode.hs
+++ src/Javelin/ByteCode.hs
@@ -15,14 +15,21 @@ data ClassBody = ClassBody {constPool :: [Constant],
                            super :: Word16}
                  deriving (Show, Eq)
 
-data Constant = Utf8 | Integer | Float | Long | Double | Class {nameIndex :: Word16} | String
+data Constant = Utf8
+              | IntegerInfo {bytes :: Word32}
+              | FloatInfo {bytes :: Word32}
+              | LongInfo {highBytes :: Word32, lowBytes :: Word32}
+              | DoubleInfo {highBytes :: Word32, lowBytes :: Word32}
+              | ClassInfo {nameIndex :: Word16}
+              | StringInfo {stringIndex :: Word16}
               | Fieldref {classIndex :: Word16, nameAndTypeIndex :: Word16}
               | Methodref {classIndex :: Word16, nameAndTypeIndex :: Word16}
               | InterfaceMethodref {classIndex :: Word16, nameAndTypeIndex :: Word16}
               | NameAndType
-              | MethodHandle | MethodType | InvokeDynamic
+              | MethodHandle
+              | MethodType
+              | InvokeDynamic
               deriving (Show, Eq)
-
 
 data Field = Field deriving (Show, Eq)
 
@@ -37,11 +44,11 @@ require len bs value = if length bs < len
                        then Left "Unexpected EOF"
                        else Right value
 
-getBytes count bs = require 2 bs $
+getBytes count bs = require count bs $
                      let high = bs !! 0
                          low = bs !! 1
                          ver = (fromIntegral high) * 256 + fromIntegral low
-                     in (drop 2 bs, ver)
+                     in (drop count bs, ver)
 
 type Parser a = [Word8] -> Either String ([Word8], a)
 
@@ -57,39 +64,6 @@ version = getBytes 2
 
 
 -- Constant pool
-constantTypeParser :: Map Int (Parser Constant)
-constantTypeParser = fromList [(1, identityParser), (3, identityParser), (4, identityParser),
-                               (5, identityParser), (6, identityParser), (7, identityParser),
-                               (8, classInfoParser), (9, fieldrefParser), (10, methodrefParser),
-                               (11, interfaceMethodrefParser), (12, identityParser), (15, identityParser),
-                               (16, identityParser), (18, identityParser)]
-getConstantParser :: Int -> Parser Constant
-getConstantParser idx = findWithDefault failingConstParser idx constantTypeParser
-
-identityParser :: Parser Constant
-identityParser bytes = Right (bytes, Utf8)
-
-failingConstParser :: Parser Constant
-failingConstParser _ = Left "Undefined constant"
-
-classInfoParser :: Parser Constant
-classInfoParser bytes = do
-  (bytes1, nameIndex) <- getBytes 2 bytes
-  return $ (bytes1, Class nameIndex)
-
-fieldrefParser = fieldMethodRefParser Fieldref
-methodrefParser = fieldMethodRefParser Methodref
-interfaceMethodrefParser = fieldMethodRefParser InterfaceMethodref
-
-fieldMethodRefParser constConstr bytes = do
-  (bytes1, classIndex) <- getBytes 2 bytes
-  (bytes2, nameAndTypeIndex) <- getBytes 2 bytes
-  return $ (bytes2, constConstr classIndex nameAndTypeIndex)
-
-getConstant bytes = do
-  (bytes1, tag) <- getBytes 1 bytes
-  getConstantParser tag bytes1
-
 getConstantPool :: Word16 -> Parser [Constant]
 getConstantPool len bytes = do
   if len == 1
@@ -98,6 +72,64 @@ getConstantPool len bytes = do
     (bytes1, pool) <- getConstant bytes
     (bytes2, pools) <- getConstantPool (len - 1) bytes1
     return (bytes2, (pool:pools))
+
+getConstant bytes = do
+  (bytes1, tag) <- getBytes 1 bytes
+  getConstantParser tag bytes1
+
+getConstantParser :: Int -> Parser Constant
+getConstantParser idx = findWithDefault failingConstParser idx constantTypeParser
+constantTypeParser :: Map Int (Parser Constant)
+constantTypeParser = fromList [(1, identityParser), (3, integerInfoParser), (4, floatInfoParser),
+                               (5, longInfoParser), (6, doubleInfoParser), (7, classInfoParser),
+                               (8, stringInfoParser), (9, fieldrefParser), (10, methodrefParser),
+                               (11, interfaceMethodrefParser), (12, identityParser), (15, identityParser),
+                               (16, identityParser), (18, identityParser)]
+                     
+identityParser :: Parser Constant
+identityParser bytes = Right (bytes, Utf8)
+
+failingConstParser :: Parser Constant
+failingConstParser _ = Left "Undefined constant"
+
+fieldrefParser = fieldMethodRefParser Fieldref
+methodrefParser = fieldMethodRefParser Methodref
+interfaceMethodrefParser = fieldMethodRefParser InterfaceMethodref
+fieldMethodRefParser constConstr bytes = do
+  (bytes1, classIndex) <- getBytes 2 bytes
+  (bytes2, nameAndTypeIndex) <- getBytes 2 bytes
+  return $ (bytes2, constConstr classIndex nameAndTypeIndex)
+
+twoBytesInfoParser :: (Word16 -> Constant) -> Parser Constant
+twoBytesInfoParser constConstr bytes = do
+  (bytes1, twoBytes) <- getBytes 2 bytes
+  return $ (bytes1, constConstr twoBytes)
+  
+classInfoParser :: Parser Constant
+classInfoParser = twoBytesInfoParser ClassInfo
+
+stringInfoParser :: Parser Constant
+stringInfoParser = twoBytesInfoParser StringInfo
+
+
+fourBytesInfoParser :: (Word32 -> Constant) -> Parser Constant
+fourBytesInfoParser constConstr bytes = do
+  (bytes1, value) <- getBytes 4 bytes
+  return $ (bytes1, constConstr value)
+integerInfoParser :: Parser Constant
+floatInfoParser :: Parser Constant
+integerInfoParser = fourBytesInfoParser IntegerInfo
+floatInfoParser = fourBytesInfoParser FloatInfo
+
+twoFourBytesInfoParser :: (Word32 -> Word32 -> Constant) -> Parser Constant
+twoFourBytesInfoParser constConstr bytes = do
+  (bytes1, bytesHigh) <- getBytes 4 bytes
+  (bytes2, bytesLow) <- getBytes 4 bytes1
+  return $ (bytes2, constConstr bytesHigh bytesLow)
+longInfoParser :: Parser Constant
+longInfoParser = twoFourBytesInfoParser LongInfo
+doubleInfoParser :: Parser Constant
+doubleInfoParser = twoFourBytesInfoParser DoubleInfo
 
 -- Interfaces
 getInterfaces :: Word16 -> Parser [Word16]
