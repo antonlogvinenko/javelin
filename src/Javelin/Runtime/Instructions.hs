6f871250b81fb98f089cd5ece20742a5cd179d55
Anton Logvinenko <logvinenko@farpost.com>
2014-04-20 13:33:58 +1100

    Memory for runtime datastructures

--- src/Javelin/Runtime/Instructions.hs
+++ src/Javelin/Runtime/Instructions.hs
@@ -5,24 +5,28 @@ import Javelin.Runtime.Thread (Thread(..),
                                Frame, Locals, Operands, ConstantPool, ProgramCounter,
                                FrameStack, Memory,
                                pool, operands, locals)
+import Data.Map.Lazy (fromList, Map)
+import Data.Word (Word8)
+
+
 
 type ThreadInstruction = Memory -> ProgramCounter -> FrameStack -> (Memory, Thread)
 type FrameInstruction = Memory -> Locals -> Operands -> ConstantPool -> (Memory, Frame)
 
-inThread :: FrameInstruction -> ThreadInstruction
-inThread instr mem pc frames = let frame1 = head frames
-                                   (mem2, frame2) = instr mem (locals frame1) (operands frame1) (pool frame1)
-                               in (mem2, Thread pc $ frame2 : tail frames)
+threadLift :: FrameInstruction -> ThreadInstruction
+threadLift instr mem pc frames = let frame1 = head frames
+                                     (mem2, frame2) = instr mem (locals frame1) (operands frame1) (pool frame1)
+                                 in (mem2, Thread pc $ frame2 : tail frames)
 
-threadInstructionsMnemonics = [()]
-frameInstructionsMnemonics = [("aaload", aaload)]
+instructions :: Word8 -> ThreadInstruction
+instructions = undefined
+-- TODO: find in any array, convert if required
 
-getInstruction :: String -> ThreadInstruction
-getInstruction = undefined
--- find in any array, convert if required
 
 
 -- Frame Instructions: locals -> stack -> pool -> newFrame
+frameInstructions :: Map Word8 FrameInstruction
+frameInstructions = fromList [(0x32, aaload), (0x53, aastore), (0x01, aconstNull)]
 
 aaload :: FrameInstruction
 aaload mem locals stack pool = undefined
@@ -33,4 +37,11 @@ aastore mem locals stack pool = undefined
 aconstNull :: FrameInstruction
 aconstNull mem locals stack pool = undefined
 
+
+
 -- Thread Instrutions
+threadInstructions :: Map Word8 ThreadInstruction
+threadInstructions = fromList [(0xba, invokedynamic)]
+
+invokedynamic :: ThreadInstruction
+invokedynamic = undefined
