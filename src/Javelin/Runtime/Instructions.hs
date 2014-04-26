d5f6b4ba42643678ef7a7424156b89948ca7f1e1
Anton Logvinenko <logvinenko@farpost.com>
2014-04-26 12:22:31 +1100

    Working on interpreting basic commands

--- src/Javelin/Runtime/Instructions.hs
+++ src/Javelin/Runtime/Instructions.hs
@@ -2,11 +2,11 @@ module Javelin.Runtime.Instructions
 where
 
 import Javelin.Runtime.Thread (Thread(..),
-                               Frame, Locals, Operands, ConstantPool, ProgramCounter,
+                               Frame(..), Locals, Operands, ConstantPool, ProgramCounter,
                                FrameStack, Memory,
                                pool, operands, locals)
 import qualified Data.Map.Lazy as Map (fromList, Map(..), union, map, (!))
-import Data.Word (Word8)
+import Data.Word (Word8, Word32, Word64)
 
 
 
@@ -28,18 +28,45 @@ instruction opcode = instructions Map.! opcode
 
 -- Frame Instructions
 frameInstructions :: Map.Map Word8 FrameInstruction
-frameInstructions = Map.fromList [(0x32, aaload), (0x53, aastore), (0x01, aconstNull)]
+frameInstructions = Map.fromList [(0x32, aaload), (0x53, aastore), (0x01, aconstNull),
 
-aaload :: FrameInstruction
+                                  (0x19, iload), (0x2a, iload_0), (0x2b, iload_1),
+                                  (0x2c, iload_2), (0x2d, iload_3),
+
+                                  (0x3a, istore), (0x4b, istore_0), (0x4c, istore_1),
+                                  (0x4d, istore_2), (0x4e, istore_3),
+
+                                  (0x93, i2s), (0x60, iadd)]
+
 aaload mem locals stack pool = undefined
-
-aastore :: FrameInstruction
 aastore mem locals stack pool = undefined
-
-aconstNull :: FrameInstruction
 aconstNull mem locals stack pool = undefined
+iadd mem locals (op1:op2:stack) pool = let sum = stackElem $ (bytes2Int op1) + (bytes2Int op2)
+                                       in (mem, Frame locals (sum:stack) pool)
+i2s mem locals (op:stack) pool = let intOp = stackElem . bytes2Short $ op
+                                 in (mem, Frame locals (intOp:stack) pool)
+
+iload = undefined
+iload_0 = undefined
+iload_1 = undefined
+iload_2 = undefined
+iload_3 = undefined
+
+istore = undefined
+istore_0 = undefined
+istore_1 = undefined
+istore_2 = undefined
+istore_3 = undefined
 
 
+bytes2Int :: Word64 -> Integer
+bytes2Int = fromIntegral
+
+bytes2Short :: Word64 -> Integer
+bytes2Short = fromIntegral
+
+stackElem :: Integer -> Word64
+stackElem = undefined
 
 -- Thread Instrutions
 threadInstructions :: Map.Map Word8 ThreadInstruction
--- src/Javelin/Runtime/Thread.hs
+++ src/Javelin/Runtime/Thread.hs
@@ -1,7 +1,7 @@
 module Javelin.Runtime.Thread
 where
 
-import Data.Word (Word8)
+import Data.Word (Word32, Word64)
 import Javelin.ByteCode.Data (Constant)
 
 data Memory = Memory
@@ -14,8 +14,8 @@ data Thread = Thread { pc :: ProgramCounter,
                        frames :: FrameStack }
               deriving (Show, Eq)
 
-type Locals = [Word8]
-type Operands = [Word8]
+type Locals = [Word32]
+type Operands = [Word64]
 type ConstantPool = [Constant]
 data Frame = Frame { locals :: Locals,
                      operands :: Operands,
