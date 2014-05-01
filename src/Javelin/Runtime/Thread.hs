779a27973c8601a668582c3d56346e24c71339d1
Anton Logvinenko <logvinenko@farpost.com>
2014-05-01 13:25:03 +1100

    JVM instructions DSL

--- src/Javelin/Runtime/Thread.hs
+++ src/Javelin/Runtime/Thread.hs
@@ -1,9 +1,12 @@
 module Javelin.Runtime.Thread
 where
 
-import Data.Word (Word32, Word64)
+import Data.Word (Word8, Word32, Word64)
 import Data.Array.IArray (Array, array)
 import Javelin.ByteCode.Data (Constant)
+import Data.Binary.Put
+import Data.Binary.Get (getWord64be, runGet)
+import Data.ByteString.Lazy (pack, ByteString)
 
 data Memory = Memory
             deriving (Show, Eq)
@@ -14,6 +17,36 @@ type FrameStack = [Frame]
 data Thread = Thread { pc :: ProgramCounter,
                        frames :: FrameStack }
               deriving (Show, Eq)
+
+
+type StartIndex = Int
+type BytesLength = Int
+class BytesContainer c where
+  getBytes :: c -> StartIndex -> BytesLength -> Word64
+
+data Arguments = Arguments { argumentsArray :: [Word8] }
+instance BytesContainer Arguments where
+  getBytes c idx len = argumentToWord64 $ take len $ drop idx $ argumentsArray c
+
+data LocalVars = LocalVars { localVariables :: [Word32] }
+instance BytesContainer LocalVars where
+  getBytes c idx len = let vars = localVariables c
+                       in if len <= 4
+                          then localToWord64 [0, vars !! 0]
+                          else localToWord64 [vars !! idx, vars !! (idx + 1)]
+
+localToWord64 :: [Word32] -> Word64
+localToWord64 bs = runGet getWord64be $ runPut $ putWord32be (bs !! 0) >> putWord32be (bs !! 1)
+
+argumentToWord64 :: [Word8] -> Word64
+argumentToWord64 bs = let normalized = (take (8 - length bs) bs) ++ bs
+                   in runGet getWord64be $ pack normalized
+
+data StackElement = StackElement { stackElement :: Word64 }
+instance BytesContainer StackElement where
+  getBytes c idx len = stackElement c
+
+
 
 
 type Locals = Array Integer Word32
