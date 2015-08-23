381fa3822bc5a64e876bde58270778e839d56417
Anton Logvinenko <anton.logvinenko@gmail.com>
2015-08-23 23:03:33 +0300

    Refactored symtable deriving

--- src/Javelin/Runtime/LLI/Loading/DeriveSymTable.hs
+++ src/Javelin/Runtime/LLI/Loading/DeriveSymTable.hs
@@ -6,35 +6,42 @@ import Javelin.Runtime.Structures
 
 import Data.Word (Word16)
 
--- 5.1 The Runtime Constant Pool
+-- 5.1 The Run-Time Constant Pool
 
 deriveSymTable :: ConstantPool -> SymTable
-deriveSymTable p = map (deriveReference p) p
+deriveSymTable p = map (deriveRef p) p
 
-deriveReference :: ConstantPool -> Constant -> SymbolicReference
+deriveRef :: ConstantPool -> Constant -> SymbolicReference
 
-deriveReference p c@(MethodHandleInfo x y) = undefined
-deriveReference p c@(InvokeDynamicInfo x y) = undefined
+deriveRef p c@(MethodHandleInfo x y) = undefined
+deriveRef p c@(InvokeDynamicInfo x y) = undefined
 
-deriveReference p c@(ClassInfo idx) = ClassOrInterface $ deriveUtf8 p idx
-deriveReference p c@(Fieldref classIdx typeIdx) =
-  FieldReference $ deriveFromClass classIdx typeIdx p
-deriveReference p c@(Methodref classIdx typeIdx) =
+deriveRef p c@(ClassInfo idx) =
+  ClassOrInterface $ deriveUtf8 p idx
+deriveRef p c@(Fieldref classIdx nameAndTypeIdx) =
+  FieldReference $ deriveFromClass classIdx nameAndTypeIdx p
+deriveRef p c@(Methodref classIdx typeIdx) =
   ClassMethodReference $ deriveFromClass classIdx typeIdx p
-deriveReference p c@(InterfaceMethodref classIdx typeIdx) =
+deriveRef p c@(InterfaceMethodref classIdx typeIdx) =
   InterfaceMethodReference $ deriveFromClass classIdx typeIdx p
-deriveReference p c@(MethodTypeInfo idx) = MethodTypeReference $ deriveUtf8 p idx
+deriveRef p c@(MethodTypeInfo idx) =
+  MethodTypeReference $ deriveUtf8 p idx
 
-deriveReference p c@(StringInfo idx) = StringLiteral $ deriveUtf8 p idx
-deriveReference p c@(DoubleInfo val) = DoubleLiteral val
-deriveReference p c@(FloatInfo val) = FloatLiteral val
-deriveReference p c@(LongInfo val) = LongLiteral val
-deriveReference p c@(IntegerInfo val) = IntegerLiteral val
+deriveRef p c@(StringInfo idx) =
+  StringLiteral $ deriveUtf8 p idx
+deriveRef p c@(DoubleInfo val) =
+  DoubleLiteral val
+deriveRef p c@(FloatInfo val) =
+  FloatLiteral val
+deriveRef p c@(LongInfo val) =
+  LongLiteral val
+deriveRef p c@(IntegerInfo val) =
+  IntegerLiteral val
 
 deriveFromClass :: (Integral i) => i -> i -> ConstantPool -> PartReference
-deriveFromClass classIdx typeIdx p =
+deriveFromClass classIdx nameAndTypeIdx p =
   let classInfo = p !! fromIntegral classIdx
-      nameAndTypeInfo = p !! fromIntegral typeIdx
+      nameAndTypeInfo = p !! fromIntegral nameAndTypeIdx
       className = stringValue $ p !! fromIntegral (nameIndex classInfo)
       methodName = stringValue $ p !! fromIntegral (nameIndex nameAndTypeInfo)
       descriptor = stringValue $ p !! fromIntegral (nameAndTypeDescriptorIndex nameAndTypeInfo)
