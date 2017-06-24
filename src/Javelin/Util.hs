67af4f195409b8aef7b73e5d9f632ad590da00a9
Anton Logvinenko <anton.logvinenko@gmail.com>
2017-06-24 22:34:23 +0300

    Dropped unused code

--- src/Javelin/Util.hs
+++ src/Javelin/Util.hs
@@ -1,7 +1,6 @@
 module Javelin.Util
 
 where
-import Control.Monad.Trans.Maybe
 
 (!?) :: (Integral i) => [a] -> i -> Maybe a
 (!?) list index = let indexInt = fromIntegral index in
@@ -13,14 +12,6 @@ import Control.Monad.Trans.Maybe
 (#) list index =
   let indexInt = fromIntegral index - 1
   in list !! indexInt
-
--- These are some missing Haskell functions, need to move somewhere else
-toMaybeT :: (Monad m) => Maybe a -> MaybeT m a
-toMaybeT = MaybeT . return
-
--- Srsly, Haskell, where is my 'replace' function?
-replace :: Char -> Char -> String -> String
-replace co cr = map (\c -> if c == co then cr else c)
 
 at :: Integral b => [a] -> b -> a
 at cc i = cc !! ((fromIntegral i) - 1)
