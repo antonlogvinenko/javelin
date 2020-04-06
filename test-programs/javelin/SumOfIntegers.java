fdc9831bd709d7b072553e12f3141954e6f27059
Anton Logvinenko <anton.logvinenko@gmail.com>
2020-04-06 19:40:56 +0300

    Add iconst_m1

--- test-programs/javelin/SumOfIntegers.java
+++ test-programs/javelin/SumOfIntegers.java
@@ -5,7 +5,9 @@ public class SumOfIntegers {
         int a = 0;
         int b = 1;
         int c = 2;
-        int d = a + b + c;
-        System.out.println(d);
+        int d = 2;
+        int e = -1;
+        int f = a + b + c + d + e;
+        System.out.println(f);
     }
 }
