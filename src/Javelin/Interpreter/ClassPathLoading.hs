782e00ce2bbe57eee358649882a180417f37992a
Anton Logvinenko <anton.logvinenko@gmail.com>
2020-08-16 15:20:57 +0300

    Fixed some imports

--- src/Javelin/Interpreter/ClassPathLoading.hs
+++ src/Javelin/Interpreter/ClassPathLoading.hs
@@ -4,13 +4,13 @@ module Javelin.Interpreter.ClassPathLoading
   , getClassBytes
   ) where
 
-import Data.ByteString.Lazy as BSL (readFile)
-import Data.List
-import Data.Map as Map (Map, empty, fromList, unions)
-import System.Directory (doesDirectoryExist, getDirectoryContents)
+import qualified Data.ByteString.Lazy as BSL (readFile)
+import qualified Data.List as List
+import qualified Data.Map as Map (Map, empty, fromList, unions)
+import qualified System.Directory as Dir (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
 
-import Codec.Archive.Zip
+import qualified Codec.Archive.Zip as Zip
 import Data.List.Split (splitOn)
 import Data.String.Utils (strip)
 import Data.Function
@@ -28,54 +28,54 @@ instance ClassPathLoading JVM where
           layout <- getClassPathLayout pathsList
           return $ ClassPathLayout layout pathsList
 
-getClassPathLayout :: [FilePath] -> IO (Map ClassName ClassSource)
-getClassPathLayout paths = unions <$> mapM getClassPathElementLayout paths
+getClassPathLayout :: [FilePath] -> IO (Map.Map ClassName ClassSource)
+getClassPathLayout paths = Map.unions <$> mapM getClassPathElementLayout paths
 
-getClassPathElementLayout :: FilePath -> IO (Map ClassName ClassSource)
+getClassPathElementLayout :: FilePath -> IO (Map.Map ClassName ClassSource)
 getClassPathElementLayout path
-  | ".class" `isSuffixOf` path = extractFileClass path
+  | ".class" `List.isSuffixOf` path = extractFileClass path
   | isZip path = extractZipClasses path
   | otherwise = do
-    isDirectory <- doesDirectoryExist path
+    isDirectory <- Dir.doesDirectoryExist path
     if not isDirectory
       then return Map.empty
       else getFilesInClassPath path >>= getClassPathLayout
 
 getFilesInClassPath :: FilePath -> IO [FilePath]
 getFilesInClassPath path = do
-  isDirectory <- doesDirectoryExist path
+  isDirectory <- Dir.doesDirectoryExist path
   if not isDirectory
     then return [path]
     else do
       files <-
         map (path </>) <$> filter (`notElem` [".", ".."]) <$>
-        getDirectoryContents path
+        Dir.getDirectoryContents path
       allFiles <- mapM getFilesInClassPath files :: IO [[FilePath]]
       return $ concat allFiles
 
 zipContentFold :: [ClassSource] -> FilePath -> [ClassSource]
 zipContentFold list path
-  | isSuffixOf ".class" path = ClassFile path : list
+  | List.isSuffixOf ".class" path = ClassFile path : list
   | isZip path = JarFile path : list
   | otherwise = list
 
-extractZipClasses :: FilePath -> IO (Map ClassName ClassSource)
+extractZipClasses :: FilePath -> IO (Map.Map ClassName ClassSource)
 extractZipClasses path = do
   raw <- BSL.readFile path
-  let arc = toArchive raw
-      allFiles = filesInArchive arc
+  let arc = Zip.toArchive raw
+      allFiles = Zip.filesInArchive arc
       paths = (map getPath $ foldl zipContentFold [] allFiles) :: [FilePath]
       s = JarFile path
   return $ Map.fromList $ (\c -> (c, s)) <$> pathToClass <$> paths
 
-extractFileClass :: FilePath -> IO (Map ClassName ClassSource)
+extractFileClass :: FilePath -> IO (Map.Map ClassName ClassSource)
 extractFileClass path =
   return
     (Map.fromList [(pathToClass (filePathToClassPath path), ClassFile path)])
 
 --Convert "main/test/App.class" to expected class path "test/App.class"
 filePathToClassPath :: String -> String
-filePathToClassPath path = path & splitOn "/" & drop 1 & intercalate "/"
+filePathToClassPath path = path & splitOn "/" & drop 1 & List.intercalate "/"
 
 --Convert "com/util/java/List.class" to "com/util/java/List"
 --Can be used to transalte class file pathes (inside jar files or on filesystem) to expected class name
@@ -83,4 +83,4 @@ pathToClass :: FilePath -> ClassName
 pathToClass path = path & splitOn ".class" & head
 
 isZip :: FilePath -> Bool
-isZip path = any (\s -> isSuffixOf s path) [".jar", ".zip", ".war", ".ear"]
+isZip path = any (\s -> List.isSuffixOf s path) [".jar", ".zip", ".war", ".ear"]
--- src/Javelin/Interpreter/Logging.hs
+++ src/Javelin/Interpreter/Logging.hs
@@ -3,7 +3,7 @@ module Javelin.Interpreter.Logging where
 import Control.Monad.Reader
 import Javelin.Capability.Classes
 import Javelin.Interpreter.JVMApp
-import Rainbow
+import qualified Rainbow as R
 import Data.Text as Text
 
 doIO :: Bool -> IO () -> JVM ()
@@ -16,7 +16,7 @@ instance Logging JVM where
     doIO logging $ writeFile "jvmDump.log" (x ++ " " ++ show a)
   console x a = do
     logging <- asks loggingMode
-    doIO logging (putChunkLn $ fore blue $ chunk $ Text.pack (x ++ " " ++ show a))
+    doIO logging (R.putChunkLn $ R.fore R.blue $ R.chunk $ Text.pack (x ++ " " ++ show a))
   say x = do
     logging <- asks loggingMode
-    doIO logging (putChunkLn $ fore green $ chunk $ Text.pack x)
+    doIO logging (R.putChunkLn $ R.fore R.green $ R.chunk $ Text.pack x)
