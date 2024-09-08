9c8c0ce2f43e155bde75dfdaec61e345d600176e
Anton Logvinenko <anton.logvinenko@gmail.com>
2024-09-08 15:55:44 +0300

    dummies for instructions

--- src/Javelin/Runtime/Instructions.hs
+++ src/Javelin/Runtime/Instructions.hs
@@ -158,32 +158,14 @@ uiShiftR n k = fromIntegral $ Bits.shiftR (fromIntegral n :: Word.Word32) k
 outReference :: JReference
 outReference = maxBound
 
+undefinedInstruction :: Global m => Thread -> m (Thread, ThreadOperation ())
+undefinedInstruction = pureInstruction empty
+
 execute :: Global m => Instruction -> Thread -> m (Thread, ThreadOperation ())
-execute Nop = pureInstruction empty
-execute BreakPoint = pureInstruction empty
-execute ImDep1 = pureInstruction empty
-execute ImDep2 = pureInstruction empty
--- resolve field -> resolve class -> load class
-execute v@(GetStatic (CPIndex index)) =
-  \t@Thread { frames = Frame { pc = pc
-                             , currentClass = classId
-                             , currentMethod = methodIndex
-                             }:_
-            , runtime = rt
-            } ->
-    let eitherSymTable = symTable <$> getClass rt classId
-     in case eitherSymTable of
-          Left err -> undefined
-          Right symTable
-            -- console "index" index
-            -- console "symTable" symTable
-           -> do
-            let classFieldReference = symTable `at` index
-            if classFieldReference == out
-              then return (t, push outReference)
-                -- then do field resolving and class init
-              else return (t, empty)
+
 -- push const <i> to stack
+execute Nop = undefinedInstruction
+execute AConstNull = undefinedInstruction
 execute IConstM1 = pureInstruction $ iconst (-1)
 execute IConst0 = pureInstruction $ iconst 0
 execute IConst1 = pureInstruction $ iconst 1
@@ -198,48 +180,111 @@ execute FConst1 = pureInstruction $ fconst 1.0
 execute FConst2 = pureInstruction $ fconst 2.0
 execute DConst0 = pureInstruction $ dconst 0.0
 execute DConst1 = pureInstruction $ dconst 1.0
--- pop int from stack, put to <i> local variable
-execute IStore0 = pureInstruction $ popAndStoreAt jint 0
-execute IStore1 = pureInstruction $ popAndStoreAt jint 1
-execute IStore2 = pureInstruction $ popAndStoreAt jint 2
-execute IStore3 = pureInstruction $ popAndStoreAt jint 3
-execute (IStore localId) = pureInstruction $ popAndStoreAt jint localId
-execute LStore0 = pureInstruction $ popAndStoreAt jlong 0
-execute LStore1 = pureInstruction $ popAndStoreAt jlong 1
-execute LStore2 = pureInstruction $ popAndStoreAt jlong 2
-execute LStore3 = pureInstruction $ popAndStoreAt jlong 3
-execute (LStore localId) = pureInstruction $ popAndStoreAt jlong localId
-execute FStore0 = pureInstruction $ popAndStoreAt jfloat 0
-execute FStore1 = pureInstruction $ popAndStoreAt jfloat 1
-execute FStore2 = pureInstruction $ popAndStoreAt jfloat 2
-execute FStore3 = pureInstruction $ popAndStoreAt jfloat 3
-execute (FStore localId) = pureInstruction $ popAndStoreAt jfloat localId
-execute DStore0 = pureInstruction $ popAndStoreAt jdouble 0
-execute DStore1 = pureInstruction $ popAndStoreAt jdouble 1
-execute DStore2 = pureInstruction $ popAndStoreAt jdouble 2
-execute DStore3 = pureInstruction $ popAndStoreAt jdouble 3
-execute (DStore localId) = pureInstruction $ popAndStoreAt jdouble localId
--- read int from local var <i>, push it to stack
+execute (BiPush value) = pureInstruction $ push (fromIntegral value :: JByte)
+execute (SiPush value) = pureInstruction $ push (fromIntegral value :: JShort)
+execute (Ldc (CPIndex idx)) = \t@Thread {frames = frame@Frame { pc = pc, currentClass = classId, currentMethod = methodIndex}:_, runtime = rt} ->
+  do
+    let eitherSymTable = symTable <$> getClass rt classId
+    case eitherSymTable of
+      Left err -> terminate "Failed to access constant pool" >> doNothing t
+      Right symTable -> do
+        let valueReference = symTable `at` idx :: SymbolicReference
+        case valueReference of
+          IntegerLiteral value -> return (t, push value)
+          FloatLiteral value -> return (t, push value)
+          _ -> terminate "not implemented yet" >> doNothing t
+execute (LdcW (CPIndex idx)) = undefinedInstruction
+execute (Ldc2W (CPIndex idx)) = \t@Thread {frames = frame@Frame { pc = pc, currentClass = classId, currentMethod = methodIndex}:_, runtime = rt} ->
+  do
+    let eitherSymTable = symTable <$> getClass rt classId
+    case eitherSymTable of
+      Left err -> terminate "Failed to access constant pool" >> doNothing t
+      Right symTable -> do
+        let valueReference = symTable `at` idx :: SymbolicReference
+        case valueReference of
+          LongLiteral value -> return (t, push value)
+          DoubleLiteral value -> return (t, push value)
+          _ -> terminate "Long value expected in constant pool" >> doNothing t
+
+-- Loads, read int from local var <i>, push it to stack
 execute (ILoad local) = pureInstruction $ loadAndPushAt jint local
+execute (LLoad local) = pureInstruction $ loadAndPushAt jlong local
+execute (FLoad local) = pureInstruction $ loadAndPushAt jfloat local
+execute (DLoad local) = pureInstruction $ loadAndPushAt jdouble local
 execute ILoad0 = pureInstruction $ loadAndPushAt jint 0
 execute ILoad1 = pureInstruction $ loadAndPushAt jint 1
 execute ILoad2 = pureInstruction $ loadAndPushAt jint 2
 execute ILoad3 = pureInstruction $ loadAndPushAt jint 3
-execute (LLoad local) = pureInstruction $ loadAndPushAt jlong local
 execute LLoad0 = pureInstruction $ loadAndPushAt jlong 0
 execute LLoad1 = pureInstruction $ loadAndPushAt jlong 1
 execute LLoad2 = pureInstruction $ loadAndPushAt jlong 2
 execute LLoad3 = pureInstruction $ loadAndPushAt jlong 3
-execute (FLoad local) = pureInstruction $ loadAndPushAt jfloat local
 execute FLoad0 = pureInstruction $ loadAndPushAt jfloat 0
 execute FLoad1 = pureInstruction $ loadAndPushAt jfloat 1
 execute FLoad2 = pureInstruction $ loadAndPushAt jfloat 2
 execute FLoad3 = pureInstruction $ loadAndPushAt jfloat 3
-execute (DLoad local) = pureInstruction $ loadAndPushAt jdouble local
 execute DLoad0 = pureInstruction $ loadAndPushAt jdouble 0
 execute DLoad1 = pureInstruction $ loadAndPushAt jdouble 1
 execute DLoad2 = pureInstruction $ loadAndPushAt jdouble 2
 execute DLoad3 = pureInstruction $ loadAndPushAt jdouble 3
+execute ALoad0 = pureInstruction $ loadAndPushAt jreference 0
+execute ALoad1 = pureInstruction $ loadAndPushAt jreference 1
+execute ALoad2 = pureInstruction $ loadAndPushAt jreference 2
+execute ALoad3 = pureInstruction $ loadAndPushAt jreference 3
+execute IaLoad = undefinedInstruction
+execute LaLoad = undefinedInstruction
+execute FaLoad = undefinedInstruction
+execute DaLoad = undefinedInstruction
+execute AaLoad = undefinedInstruction
+execute BaLoad = undefinedInstruction
+execute CaLoad = undefinedInstruction
+execute SaLoad = undefinedInstruction
+
+-- Stores, pop int from stack, put to <i> local variable
+execute (IStore localId) = pureInstruction $ popAndStoreAt jint localId
+execute (LStore localId) = pureInstruction $ popAndStoreAt jlong localId
+execute (FStore localId) = pureInstruction $ popAndStoreAt jfloat localId
+execute (DStore localId) = pureInstruction $ popAndStoreAt jdouble localId
+execute (AStore localId) = undefinedInstruction
+execute IStore0 = pureInstruction $ popAndStoreAt jint 0
+execute IStore1 = pureInstruction $ popAndStoreAt jint 1
+execute IStore2 = pureInstruction $ popAndStoreAt jint 2
+execute IStore3 = pureInstruction $ popAndStoreAt jint 3
+execute LStore0 = pureInstruction $ popAndStoreAt jlong 0
+execute LStore1 = pureInstruction $ popAndStoreAt jlong 1
+execute LStore2 = pureInstruction $ popAndStoreAt jlong 2
+execute LStore3 = pureInstruction $ popAndStoreAt jlong 3
+execute FStore0 = pureInstruction $ popAndStoreAt jfloat 0
+execute FStore1 = pureInstruction $ popAndStoreAt jfloat 1
+execute FStore2 = pureInstruction $ popAndStoreAt jfloat 2
+execute FStore3 = pureInstruction $ popAndStoreAt jfloat 3
+execute DStore0 = pureInstruction $ popAndStoreAt jdouble 0
+execute DStore1 = pureInstruction $ popAndStoreAt jdouble 1
+execute DStore2 = pureInstruction $ popAndStoreAt jdouble 2
+execute DStore3 = pureInstruction $ popAndStoreAt jdouble 3
+execute AStore0 = pureInstruction $ popAndStoreAt jreference 0
+execute AStore1 = pureInstruction $ popAndStoreAt jreference 0
+execute AStore2 = pureInstruction $ popAndStoreAt jreference 0
+execute AStore3 = pureInstruction $ popAndStoreAt jreference 0
+execute IaStore = undefinedInstruction
+execute LaStore = undefinedInstruction
+execute FaStore = undefinedInstruction
+execute DaStore = undefinedInstruction
+execute AaStore = undefinedInstruction
+execute BaStore = undefinedInstruction
+execute CaStore = undefinedInstruction
+execute SaStore = undefinedInstruction
+-- Stack
+execute Pop = undefinedInstruction
+execute Pop2 = undefinedInstruction
+execute Dup = undefinedInstruction
+execute DupX1 = undefinedInstruction
+execute DupX2 = undefinedInstruction
+execute Dup2 = undefinedInstruction
+execute Dup2X1 = undefinedInstruction
+execute Dup2X2 = undefinedInstruction
+execute Swap = undefinedInstruction
+-- Math
 execute IAdd = pureInstruction $ math (+) jint
 execute LAdd = pureInstruction $ math (+) jlong
 execute FAdd = pureInstruction $ math (+) jfloat
@@ -268,39 +313,92 @@ execute LXor = pureInstruction $ math Bits.xor jlong
 execute LShl = pureInstruction $ shift Bits.shiftL jlong
 execute LShr = pureInstruction $ shift Bits.shiftR jlong
 execute LUshr = pureInstruction $ shift ulShiftR jlong
-execute (BiPush value) = pureInstruction $ push (fromIntegral value :: JByte)
 execute IDiv = pureInstruction $ math div jint
 execute LDiv = pureInstruction $ math div jlong
 execute FDiv = pureInstruction $ math (/) jfloat
 execute DDiv = pureInstruction $ math (/) jdouble
 execute IRem = pureInstruction $ math rem jint
 execute LRem = pureInstruction $ math rem jlong
-execute FRem = undefined
-execute DRem = undefined
-execute (Ldc2W (CPIndex idx)) = \t@Thread {frames = frame@Frame { pc = pc, currentClass = classId, currentMethod = methodIndex}:_, runtime = rt} ->
-  do
-    let eitherSymTable = symTable <$> getClass rt classId
-    case eitherSymTable of
-      Left err -> terminate "Failed to access constant pool" >> doNothing t
-      Right symTable -> do
-        let valueReference = symTable `at` idx :: SymbolicReference
-        case valueReference of
-          LongLiteral value -> return (t, push value)
-          DoubleLiteral value -> return (t, push value)
-          _ -> terminate "Long value expected in constant pool" >> doNothing t
-execute (Ldc (CPIndex idx)) = \t@Thread {frames = frame@Frame { pc = pc, currentClass = classId, currentMethod = methodIndex}:_, runtime = rt} ->
-  do
-    let eitherSymTable = symTable <$> getClass rt classId
-    case eitherSymTable of
-      Left err -> terminate "Failed to access constant pool" >> doNothing t
-      Right symTable -> do
-        let valueReference = symTable `at` idx :: SymbolicReference
-        case valueReference of
-          IntegerLiteral value -> return (t, push value)
-          FloatLiteral value -> return (t, push value)
-          _ -> terminate "not implemented yet" >> doNothing t
+execute FRem = undefinedInstruction
+execute DRem = undefinedInstruction
+execute (IInc localId word8) = undefinedInstruction 
 
+-- Conversions
+execute I2L = undefinedInstruction
+execute I2F = undefinedInstruction
+execute I2D = undefinedInstruction
+execute L2I = undefinedInstruction
+execute L2F = undefinedInstruction
+execute L2D = undefinedInstruction
+execute F2I = undefinedInstruction
+execute F2L = undefinedInstruction
+execute F2D = undefinedInstruction
+execute D2I = undefinedInstruction
+execute D2L = undefinedInstruction
+execute D2F = undefinedInstruction
+execute I2B = undefinedInstruction
+execute I2C = undefinedInstruction
+execute I2S = undefinedInstruction
+  
+-- Comparisons
+execute LCmp = undefinedInstruction
+execute FCmpL = undefinedInstruction
+execute FCmpG = undefinedInstruction
+execute DCmpL = undefinedInstruction
+execute DCmpG = undefinedInstruction
+execute (IfEq branchOffset) = undefinedInstruction
+execute (IfNe branchOffset) = undefinedInstruction
+execute (IfLt branchOffset) = undefinedInstruction
+execute (IfGe branchOffset) = undefinedInstruction
+execute (IfGt branchOffset) = undefinedInstruction
+execute (IfLe branchOffset) = undefinedInstruction
+execute (IfICmpEq branchOffset) = undefinedInstruction
+execute (IfICmpNe branchOffset) = undefinedInstruction
+execute (IfICmpLt branchOffset) = undefinedInstruction
+execute (IfICmpGe branchOffset) = undefinedInstruction
+execute (IfICmpGt branchOffset) = undefinedInstruction
+execute (IfICmpLe branchOffset) = undefinedInstruction
+execute (IfACmpEq branchOffset) = undefinedInstruction
+execute (IfACmpNe branchOffset) = undefinedInstruction
+
+-- Control
+execute (Goto branchOffset) = undefinedInstruction
+execute (Jsr branchOffset) = undefinedInstruction
+execute (Ret localId) = undefinedInstruction
+execute (TableSwitch a b c d) = undefinedInstruction
+execute (LookupSwitch a b) = undefinedInstruction
+execute IReturn = undefinedInstruction
+execute LReturn = undefinedInstruction
+execute FReturn = undefinedInstruction
+execute DReturn = undefinedInstruction
+execute AReturn = undefinedInstruction
 execute Return = pureInstruction dropTopFrame
+
+-- References
+-- resolve field -> resolve class -> load class
+execute v@(GetStatic (CPIndex index)) =
+  \t@Thread { frames = Frame { pc = pc
+                             , currentClass = classId
+                             , currentMethod = methodIndex
+                             }:_
+            , runtime = rt
+            } ->
+    let eitherSymTable = symTable <$> getClass rt classId
+     in case eitherSymTable of
+          Left err -> undefined
+          Right symTable
+            -- console "index" index
+            -- console "symTable" symTable
+           -> do
+            let classFieldReference = symTable `at` index
+            if classFieldReference == out
+              then return (t, push outReference)
+                -- then do field resolving and class init
+              else return (t, empty)
+execute (PutStatic cpIndex) = undefinedInstruction
+execute (GetField cpIndex) = undefinedInstruction
+execute (PutField cpIndex) = undefinedInstruction
+
 execute (InvokeVirtual (CPIndex index)) =
   \t@Thread { frames = (frame@Frame { pc = pc
                                     , currentClass = classId
@@ -325,6 +423,44 @@ execute (InvokeVirtual (CPIndex index)) =
                if outReference == object
                  then MonadIO.liftIO $ putStr value
                  else empty)
+execute (InvokeSpecial cpIndex) = undefinedInstruction
+execute (InvokeStatic cpIndex) = undefinedInstruction
+execute (InvokeInterface cpIndex idx) = undefinedInstruction
+execute (InvokeDynamic cpIndex idx) = undefinedInstruction
+execute (New_ cpIndex) = undefinedInstruction
+execute (NewArray word8) = undefinedInstruction
+execute (ANewArray cpIndex) = undefinedInstruction
+execute ArrayLength = undefinedInstruction
+execute AThrow = undefinedInstruction
+execute (CheckCast cpIndex) = undefinedInstruction
+execute (InstanceOf_ cpIndex) = undefinedInstruction
+execute MonitorEnter = undefinedInstruction
+execute MonitorExit = undefinedInstruction
+
+-- Extended
+execute (WideIInc cpIndex word16) = undefinedInstruction
+execute (WideILoad cpIndex) = undefinedInstruction
+execute (WideFLoad cpIndex) = undefinedInstruction
+execute (WideALoad cpIndex) = undefinedInstruction
+execute (WideLLoad cpIndex) = undefinedInstruction
+execute (WideDLoad cpIndex) = undefinedInstruction
+execute (WideIStore cpIndex) = undefinedInstruction
+execute (WideFStore cpIndex) = undefinedInstruction
+execute (WideAStore cpIndex) = undefinedInstruction
+execute (WideLStore cpIndex) = undefinedInstruction
+execute (WideDStore cpIndex) = undefinedInstruction
+execute (WideRet cpIndex) = undefinedInstruction
+execute (MultiANewArray cpIndex word8) = undefinedInstruction
+execute (IfNull cpIndex) = undefinedInstruction
+execute (IfNotNull cpIndex) = undefinedInstruction
+execute (GotoW word32) = undefinedInstruction
+execute (JsrW word32) = undefinedInstruction
+
+-- Reserved
+execute BreakPoint = undefinedInstruction
+execute ImDep1 = undefinedInstruction
+execute ImDep2 = undefinedInstruction
+
 
 typeFormatter :: String -> ThreadOperation String
 typeFormatter x
